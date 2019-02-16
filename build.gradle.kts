b58734f2db3285cd03e62c44f22b58eb5a0c5e82
Martin Vysny <martin@vysny.me>
2019-02-16 13:36:26 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
     // test support
     testCompile("com.github.mvysny.kaributesting:karibu-testing-v10:1.1.2")
-    testCompile("com.github.mvysny.dynatest:dynatest-engine:0.13")
+    testCompile("com.github.mvysny.dynatest:dynatest-engine:0.15")
 
     // heroku app runner
     staging("com.github.jsimone:webapp-runner:9.0.14.0")
