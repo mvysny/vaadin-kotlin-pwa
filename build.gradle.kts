0af7ec980c56b68689cf9be07b3edca2f7aa89c8
Martin Vysny <mavi@vaadin.com>
2021-02-04 19:29:58 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val vaadinonkotlin_version = "0.9.0"
-val vaadin10_version = "14.4.3"
+val vaadinonkotlin_version = "0.9.1"
+val vaadin10_version = "14.4.7"
 val slf4j_version = "1.7.30"
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.30"
     id("org.gretty") version "3.0.3"
     war
     id("com.vaadin") version "0.14.3.7"
@@ -67,7 +67,7 @@ dependencies {
     implementation("mysql:mysql-connector-java:5.1.48")
 
     // test support
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.2.6")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.2.8")
     testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.19")
 
     // heroku app runner
