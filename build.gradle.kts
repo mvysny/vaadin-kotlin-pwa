cd8a3ac316cfab8132169a8141bae02ca472b38a
Martin Vysny <mavi@vaadin.com>
2021-10-22 17:09:58 +0300

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
 
     // test support
     testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.3.4")
-    testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.21")
+    testImplementation("com.github.mvysny.dynatest:dynatest:0.22")
 
     // heroku app runner
     staging("com.heroku:webapp-runner:9.0.52.0")
