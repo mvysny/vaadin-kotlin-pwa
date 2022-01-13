46456d9b1f9e8302de5772b955c46f8348f291da
Martin Vysny <mavi@vaadin.com>
2022-01-13 18:54:48 +0200

    run on jdk 11+

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ tasks.withType<Test> {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 val staging by configurations.creating
@@ -76,8 +76,8 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 // Heroku
