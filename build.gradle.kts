ae244f950a1f1d1d5af2d8c8634150bd12d881da
Martin Vysny <martin@vysny.me>
2026-09-16 20:33:10 +0300

    kotlin bump to 2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.4.10"
+    kotlin("jvm") version "2.4.20"
     application
     alias(libs.plugins.vaadin)
 }
